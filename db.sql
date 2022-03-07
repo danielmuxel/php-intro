d44184e215a07324a8e0ec332084e5bca68eaa4a
Daniel Muxel <daniel.muxel@gmail.com>
2022-03-07 20:19:18 +0100

    initial commit

---
+++ db.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS people;
+CREATE DATABASE IF NOT EXISTS people;
+
+CREATE TABLE country (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  firstname TEXT NOT NULL,
+  lastname TEXT NOT NULL,
+  birthdate DATE NOT NULL,
+  image TEXT NOT NULL,
+  country_id INTEGER NOT NULL,
+  FOREIGN KEY(country_id) REFERENCES country(id)
+);
+
+INSERT INTO person (firstname, lastname, birthdate, image)
+VALUES ('John', 'Doe', '2000-01-01', 'john.jpg'),
+('Jane', 'Doe', '2000-01-01', 'jane.jpg'),
+('Jack', 'Doe', '2000-01-01', 'jack.jpg'),
+('Jill', 'Doe', '2000-01-01', 'jill.jpg');
